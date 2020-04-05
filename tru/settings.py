c5cce776e654cfbdfab3a6e322ee5b521e379d60
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-04-05 13:46:32 -0700

    debug again

--- tru/settings.py
+++ tru/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'tru.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 
-if DEBUG:
+#if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'trudb',
-            'USER': 'trudb_user',
-            'PASSWORD': 'truDB_password',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+#else:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': 'trudb',
+#            'USER': 'trudb_user',
+#            'PASSWORD': 'truDB_password',
+#            'HOST': 'localhost',
+#            'PORT': '',
+#        }
+#    }
 
 # this database is available to us, it just needs some configuration in djago before switching
 #DATABASES = {
