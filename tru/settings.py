c7c5de08d87f8a7ffa2a28f143c6426c9d68fa0c
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-04-05 09:50:49 -0700

    static root only now

--- tru/settings.py
+++ tru/settings.py
@@ -135,16 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
-)
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Custom Django auth settings
 
 AUTH_USER_MODEL = 'classroom.User'
