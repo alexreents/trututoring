22d8879c11244ad4cf985af787b83a61b29486a4
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-04-05 10:10:00 -0700

    new way

--- tru/settings.py
+++ tru/settings.py
@@ -135,8 +135,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static/'),
+)
 
 # Custom Django auth settings
 
