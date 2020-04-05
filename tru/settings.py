2b5fec57afa3920df34caafbf7b9d1d209b2d3fb
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-04-05 10:59:52 -0700

    again

--- tru/settings.py
+++ tru/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, '../static_resources')
 
 STATIC_URL = '/static/'
 
