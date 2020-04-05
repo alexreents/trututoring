0bf9567499c4d54bc3eb702ee264b97b6003162b
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-04-05 14:04:16 -0700

    debugn

--- tru/settings.py
+++ tru/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'tru.wsgi.application'
 
 
 #if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 #else:
 #    DATABASES = {
 #        'default': {
