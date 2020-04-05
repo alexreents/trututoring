dd43a13668520cd8c7fcc039b8290580194e5a80
Alex Reents <alexreents@Alexs-MacBook-Pro.local>
2020-04-04 23:05:32 -0700

    static settings fix

--- tru/settings.py
+++ tru/settings.py
@@ -135,8 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Custom Django auth settings
 
 AUTH_USER_MODEL = 'classroom.User'
